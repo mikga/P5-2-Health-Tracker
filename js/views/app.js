8e4991b36963e186b80f0a5aae82557ef7d00b34
mikga <obamikio+ga@gmail.com>
2015-10-28 23:04:17 +0000

    Add basic food search functionality

--- js/views/app.js
+++ js/views/app.js
@@ -9,7 +9,8 @@ app.AppView = Backbone.View.extend({
   totalCaloriesTemplate: _.template($('#total-calories').html()),
 
   events: {
-    'keypress .new-food-name': 'createOnEnter',
+    // 'keypress .new-food-name': 'createOnEnter',
+    'keyup .new-food-name': 'searchFood',
     'keypress .new-food-amount': 'createOnEnter',
     'keypress .new-food-calorie': 'createOnEnter'
   },
@@ -78,6 +79,48 @@ app.AppView = Backbone.View.extend({
     this.$foodname.val('');
     this.$foodamount.val('');
     this.$foodcalorie.val('');
+  },
+
+  searchFood: function() {
+    var name = this.$foodname.val().trim();
+    var url = 'https://api.nutritionix.com/v1_1/search/' + name + '?fields=item_name%2Citem_id%2Cbrand_name%2Cnf_calories%2Cnf_serving_size_unit%2Cnf_serving_size_qty%2Cnf_serving_weight_grams&appId=ab67ebd5&appKey=ff8d79e60c8d9447ddf0457786be4f77';
+    $('.search-results').html('');
+
+    console.log('Search term: ' + name);
+
+    if (name) {
+
+      $.ajax({
+        url: url
+      }).done(function(msg) {
+        var itemName,
+            itemId,
+            brandName,
+            nfCalories,
+            nfServingSizeUnit,
+            nfServingSizeQty,
+            nfServingWeightGrams;
+
+        var items = msg.hits;
+
+        for (var i = 0, len = items.length; i < len; i++){
+          itemName = items[i].fields.item_name;
+          itemId = items[i].fields.item_id;
+          brandName = items[i].fields.brand_name;
+          nfCalories = items[i].fields.nf_calories;
+          nfServingSizeUnit = items[i].fields.nf_serving_size_unit;
+          nfServingSizeQty = items[i].fields.nf_serving_size_qty;
+          nfServingWeightGrams = items[i].fields.nf_serving_weight_grams;
+
+          $('.search-results').append('<li class="search-result-item"><span class="item-name">' + itemName + '</span> / <span class="brand-name">' + brandName + '</span></li>');
+          // console.log('Item name: ' + itemName + ', brandName: ' + brandName + ', nf_calories: ' + nfCalories + ', nf_serving_size_unit: ' + nfServingSizeUnit + ', nf_serving_size_qty: ' + nfServingSizeQty + ', nf_serving_weight_grams: ' + nfServingWeightGrams);
+        }
+
+      }).fail(function(jqXHR, textStatus) {
+        console.log( "Request failed: " + textStatus );
+      });
+
+    }
   }
 
 });
